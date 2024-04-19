c404506320fbd38cc3c20fcff5922e7b69005af2
EC2 Default User <ec2-user@ip-172-31-33-215.ap-northeast-1.compute.internal>
2024-04-19 08:48:10 +0000

    いいね非同期

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,7 @@ class Book < ApplicationRecord
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
+	end
 	
   def self.search_for(content, method)
     if method == 'perfect'
@@ -20,5 +21,5 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%'+content+'%')
     end
   end
-  end
+  
 end
